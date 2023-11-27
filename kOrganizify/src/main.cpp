196d462d91f84a5c3fefaf331987366d8a81db3d
andjixi <andjela_07@hotmail.com>
2023-11-27 23:38:57 +0100

    modified CMakeLists.txt

--- kOrganizify/src/main.cpp
+++ kOrganizify/src/main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
+#include "appwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+//    MainWindow w;
+//    w.show();
+
+    AppWindow aw;
+    aw.show();
+
+
     return a.exec();
 }
