92a942eed51603d6bbd327cc5b1dfe3caafa7fe8
Marko Paunovic <markopaunovic1414@gmail.com>
2023-12-10 16:01:46 +0100

    modified settings gui

--- kOrganizify/src/main.cpp
+++ kOrganizify/src/main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "appwindow.h"
 #include "eventwindow.h"
+#include "settingswindow.h"
 
 #include <QApplication>
 
@@ -15,6 +16,9 @@ int main(int argc, char *argv[])
 
     // EventWindow ew;
     // ew.show();
+
+    SettingsWindow sw;
+    sw.show();
 
     return a.exec();
 }
