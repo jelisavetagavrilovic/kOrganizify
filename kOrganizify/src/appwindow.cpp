975649fffa4854858f92f6f137856f39ce2b7bfe
Marko Paunovic <markopaunovic1414@gmail.com>
2023-12-12 22:19:53 +0100

    made app more pretty

--- kOrganizify/src/appwindow.cpp
+++ kOrganizify/src/appwindow.cpp
@@ -1,6 +1,7 @@
 #include "appwindow.h"
 #include "ui_appwindow.h"
 #include "settingswindow.h"
+#include <QPixmap>
 
 AppWindow::AppWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -9,10 +10,16 @@ AppWindow::AppWindow(QWidget *parent)
     ui->setupUi(this);
     this->settingsWindow = new SettingsWindow(this);
     this->settingsWindow->setColor("#0050B5");
+    this->ui->lwToDoList->setStyleSheet("background-color: #FCD299");
+    QPixmap background(":/images/background1.jpg");
+    QPalette palette;
+    palette.setBrush(this->backgroundRole(), QBrush(background));
+    this->setPalette(palette);
 
     QString styleSheet = QString("background-color: %1").arg(this->settingsWindow->getColor());
     this->ui->btnSettings->setStyleSheet(styleSheet);
-    this->ui->btnSettings->update();
+    this->ui->leInput->setStyleSheet(styleSheet);
+    this->ui->lblToDoList->setStyleSheet("color: " + this->settingsWindow->getColor());
 
     connect(ui->btnSettings, &QPushButton::clicked, this, &AppWindow::on_btnSettings_clicked);
     connect(settingsWindow, &SettingsWindow::colorChanged, this, &AppWindow::changeButtonColor);
@@ -20,6 +27,8 @@ AppWindow::AppWindow(QWidget *parent)
 
 void AppWindow::changeButtonColor(const QString& newColor) {
     this->ui->btnSettings->setStyleSheet("background-color: " + newColor);
+    this->ui->leInput->setStyleSheet("background-color: " + newColor);
+    this->ui->lblToDoList->setStyleSheet("color: " + newColor);
 }
 
 void AppWindow::on_btnSettings_clicked()
